cdc321af4148be72149ab2286ea0008173df8a0f
Li11iana <liliv.azofeifa@gmail.com>
2022-11-10 23:35:38 -0800

    Completed deliverable 3, bonus & readme pending

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -1,4 +1,7 @@
-﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+
+
+
+-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
 -- Link to schema: https://app.quickdatabasediagrams.com/#/d/RX0d4C
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
@@ -71,4 +74,35 @@ REFERENCES "subcategory" ("subcategory_id");
 
 ALTER TABLE "backers" ADD CONSTRAINT "fk_backers_cf_id" FOREIGN KEY("cf_id")
 REFERENCES "campaign" ("cf_id");
+
+-- Challenge Bonus queries.
+-- 1. (2.5 pts)
+-- Retrieve all the number of backer_counts in descending order for each `cf_id` for the "live" campaigns. 
+
+
+
+-- 2. (2.5 pts)
+-- Using the "backers" table confirm the results in the first query.
+
+
+
+-- 3. (5 pts)
+-- Create a table that has the first and last name, and email address of each contact.
+-- and the amount left to reach the goal for all "live" projects in descending order. 
+
+
+
+
+-- Check the table
+
+
+-- 4. (5 pts)
+-- Create a table, "email_backers_remaining_goal_amount" that contains the email address of each backer in descending order, 
+-- and has the first and last name of each backer, the cf_id, company name, description, 
+-- end date of the campaign, and the remaining amount of the campaign goal as "Left of Goal". 
+
+
+
+-- Check the table
+
 
